42d1245360793f917a01d436674652d104032c3b
Karl Ghanem <Karl.Ghanem@EITECH.LOCAL>
2026-06-23 10:27:57 +0300

    Testing second branch

--- force-app/main/default/lwc/promo10First/promo10First.js
+++ force-app/main/default/lwc/promo10First/promo10First.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 
 export default class Promo10First extends LightningElement {
-    greeting = "World";
+    greeting = "World!";
+    name = " This karl";
 }
